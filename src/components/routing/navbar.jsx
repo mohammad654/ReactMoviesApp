d3a66bb78223e4483dbd6a47fa182ce43d5a0473
Mohammad Ali <Mohammad.ali.shikhi.55@gmail.com>
2022-12-29 23:51:07 +0100

    Route

--- src/components/routing/navbar.jsx
+++ src/components/routing/navbar.jsx
@@ -14,7 +14,7 @@ const NavBar = () => {
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav">
           <li className="nav-item">
-            <NavLink className="nav-link" to="/">Home</NavLink>
+            <NavLink className="nav-link" to="/home">Home</NavLink>
           </li>
           <li className="nav-item">
             <NavLink className="nav-link active" aria-current="page" to="/movies">Movies</NavLink>
